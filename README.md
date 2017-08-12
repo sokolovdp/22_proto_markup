8506832163bfff2aa1fb0ef8170ea2d21d225279
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-08-12 21:17:40 +0300

    1st commit 2 pages: index & orders

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Suppliers in Novosibirsk
 
-[TODO. There will be project description]
+Frontend static demo site["Suppliers of Novosibirsk"](https://sokolovdp.github.io)
+Static pages were generates from templates using **staticjinja ver 0.3.0**  package. 
+
+# How to launch site generator:
+```
+python3 generator.py
+```
+Demo site pages will be created in folder `demo`.
 
 # Project Goals
 
---
+++ generator.py
@@ -0,0 +1,32 @@
+from staticjinja import make_site
+
+sample_order = {
+    'time': 'Вчера, в 21:30',
+    'text': '60 шт. ПК от 72-15 до ПК 21-15, Криводановка, с доставкой',
+    'name': 'Алексей',
+    'views': 12}
+
+sample_comment = {
+    'name': 'Кирилл',
+    'age': 29,
+    'city': 'Барабинск',
+    'text': 'Бла бла... мне помогло, все супер! ' * 7}
+
+index_context = {
+    'user': 'Дмитрий Соколов',
+    'region': 'Новосибирск и область',
+    'orders': [sample_order] * 3,
+    'comments': [sample_comment] * 3,
+    'index_page': True}
+
+orders_context = {
+    'region': 'Новосибирск и область',
+    'orders': [sample_order] * 11,
+    'pages':  11,
+    'index_page': False}
+
+if __name__ == "__main__":
+    site = make_site(outpath='demo', contexts=[
+        ('index.html', index_context),
+        ('orders.html', orders_context)])
+    site.render()
